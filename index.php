cc2e1e31b9dc24087cf613fd15b9eaa0c7d54987
CARLOS HENRIQUE MORAES DOS SANTOS <carlossantos.gp54@gmail.com>
2026-06-24 22:31:37 -0300

    Reformulada na tela de login

--- index.php
+++ index.php
@@ -11,18 +11,25 @@
     <div id="background">
     <div id="light"></div>
         <main>
-            <form method="post" action="logar.php">
-                <h1>Login</h1>
-                <div class="inputBox">
-                    <label for="email">E-mail</label>
-                    <input name="email" type="text">
-                </div>
-                <div class="inputBox">
-                    <label for="password">Senha</label>
-                    <input name="password" type="password">
-                </div>
-                <button type="submit">Login</button>
-            </form>
+            <div class="form">
+                <form method="post" action="logar.php">
+                    <h1>Login</h1>
+                    <div class="inputBox">
+                        <label for="email">E-mail</label>
+                        <input name="email" type="text">
+                    </div>
+                    <div class="inputBox">
+                        <label for="password">Senha</label>
+                        <input name="password" type="password">
+                    </div>
+                    <button type="submit">Login</button>
+                    <div class="links">
+                        <a href="cadastro.php">Não tenho conta</a>
+                        <a href="#">Esqueci minha senha</a>
+                    </div>
+                </form>
+                <img src="./assets/images/login-image.png" alt="Imagem de login">
+            </div>
         </main>
     </div>
     <script src="./scripts/login.js"></script>
--- pages/cadastro.php
+++ pages/cadastro.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda</title>
+    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="./styles/cadastro.css">
 </head>
 <body>
-    
+    <div id="background">
+    <div id="light"></div>
+        <main>
+            <form method="post" action="logar.php">
+                <h1>Login</h1>
+                <div class="inputBox">
+                    <label for="email">E-mail</label>
+                    <input name="email" type="text">
+                </div>
+                <div class="inputBox">
+                    <label for="password">Senha</label>
+                    <input name="password" type="password">
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </main>
+    </div>
+    <script src="./scripts/cadastro.js"></script>
 </body>
 </html>
